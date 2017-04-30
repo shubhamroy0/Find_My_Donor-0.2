1eccdb74a69164b08f5921e127398798ee433d0b
Sairaorg209 <arnbsdkn13@gmail.com>
2017-04-30 20:06:19 +0530

    Adding php classes Requester and Request

---
+++ project3/php/Request.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<?php
+ class Request {
+      var $amountofBlood;
+	  var $bloodgroup;
+	  
+	   function setBloodType( $par1 ) {
+	   $this->bloodgroup = $par1;
+	   }  
+}	  
+?>
---
+++ project3/php/Requester.php
@@ -0,0 +1,113 @@
+<!DOCTYPE html>
+<?php
+
+include 'php/Request.php';
+
+ class Requester {
+      var $firstname;
+	  var $lastname;
+	  var $email_id;
+	  var $mobile;
+	  var $age;
+	  var $latitude;
+	  var $longitude;
+	  var $bloodDetails;
+   
+    function trackLocation($lat, $lon){
+		$this->latitude=$lat;
+		$this->longitude=$lon;
+      }
+	
+	function calDistance($lat2, $lon2) {
+					
+				  $lat1=$this->getLatitude();
+				  $lon1=$this->getLongitude();	
+				  
+				  $theta = $lon1 - $lon2;
+				  $dist = sin(deg2rad($lat1)) * sin(deg2rad($lat2)) +  cos(deg2rad($lat1)) * cos(deg2rad($lat2)) * cos(deg2rad($theta));
+				  $dist = acos($dist);
+				  $dist = rad2deg($dist);
+				  $miles = $dist * 60 * 1.1515;
+				  $kilometers = $miles * 1.609344;
+				  
+				  return $kilometers;  
+				}
+				
+	function searchBlood($bloodDetails){
+		include 'db.php';
+		$index = 0;
+		$selId = '(0';
+		
+		
+		$result = $mysqli->query("SELECT * 
+		FROM users u
+		WHERE u.blood_group = '$bloodDetails->bloodgroup'")  or die($mysqli->error());
+		
+		
+		while($row=mysqli_fetch_array($result)){
+			$dist = $this->calDistance($row['latitude'],$row['longitude']);
+			if($dist<=5.0){
+				$selId = $selId.','.$row['id'];
+				
+			}
+		}
+		
+		$selId = $selId.')';
+		echo $selId;
+		$result = $mysqli->query("SELECT * 
+		FROM users 
+		WHERE id in $selId")  or die($mysqli->error());
+		
+		return $result;
+		
+         
+      }	
+	
+	function notifyDonor($par){
+         
+      }
+	
+	function chooseHospital($par){
+         
+      }
+	  
+	function getFirstName(){
+        echo $this->firstname; 
+      }
+	
+	function getLastName(){
+        echo $this->lastname; 
+      }	
+	  
+	function getEmailId(){
+        echo $this->email_id; 
+      }
+	  
+	function getMobile(){
+        echo $this->mobile;         
+      }
+	  
+	function getAge(){
+        echo $this->age;         
+      }
+	  
+	function getLatitude(){
+        return $this->latitude;         
+      }
+	  
+	function getLongitude(){
+        return $this->longitude;         
+      }
+	  
+   function __construct( $par1, $par2, $par3, $par4, $par5, $par6 ) {
+   $this->firstname = $par1;
+   $this->lastname = $par2;
+   $this->email_id = $par3;
+   $this->mobile = $par4;
+   $this->age = $par5;
+   $this->bloodDetails = new Request();
+   $this->bloodDetails->setBloodType($par6);
+	}
+ }	
+	  
+?>
